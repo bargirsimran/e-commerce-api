ccc2ad1266a7f90b9d32b0ddf7dbc45cc48233c5
simranbargir <simranbargir007@gmail.com>
2024-07-05 23:40:20 +0530

    Setup basic project structure and configuration for Spring Boot e-commerce API

---
+++ src/main/java/org/dnyanyog/EcommerceApiMain.java
@@ -0,0 +1,12 @@
+package org.dnyanyog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceApiMain {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceApiMain.class, args);
+	}
+}
